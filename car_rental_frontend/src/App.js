69cc43e0011e8bcb5723ff1a4e0fd434a8e7e2a3
eva.wang <eva.wang81@gmail.com>
2022-08-03 12:32:43 +0200

    updated app and car component

--- car_rental_frontend/src/App.js
+++ car_rental_frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home.js";
 import Bookings from "./routes/Bookings.js";
@@ -7,47 +7,18 @@ import About from "./routes/About.js";
 import Car from "./routes/Car.js";
 import Book from "./routes/Book.js";
 import NotFound from "./routes/NotFound.js";
-import CarsContext from "./providers/CarsContext";
-import axios from "axios";
-
-axios.defaults.withCredentials = true;
-
-const BACKEND_URL =
-  process.env.REACT_APP_CAR_RENTAL_BACKEND_URL || "http://localhost:3004";
 
 function App() {
-  const [allCarsList, setAllCarsList] = useState();
-  console.log("home all cars: ", allCarsList);
-
-  useEffect(() => {
-    const getAllCars = async () => {
-      try {
-        console.log("called useEffect");
-        const result = await axios.get(`${BACKEND_URL}/allCars`);
-
-        console.log(result);
-        setAllCarsList(result.data.allCars);
-      } catch (error) {
-        console.log("Error message: ", error);
-      }
-    };
-    getAllCars();
-  }, []);
-
-  console.log(CarsContext);
-
   return (
     <Router>
-      <CarsContext.Provider value={allCarsList}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Bookings" element={<Bookings />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/cars/:id" element={<Car />} />
-          <Route path="/cars/:id/book" element={<Book />}></Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </CarsContext.Provider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Bookings" element={<Bookings />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/cars/:id" element={<Car />} />
+        <Route path="/cars/:id/book" element={<Book />}></Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }
--- car_rental_frontend/src/routes/Car.js
+++ car_rental_frontend/src/routes/Car.js
@@ -1,17 +1,39 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import CarsContext from "../providers/CarsContext";
-import { Link } from "react-router-dom";
+// import CarsContext from "../providers/CarsContext";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+
+// make sure that axios always sends the cookies to the backend server
+axios.defaults.withCredentials = true;
+
+const BACKEND_URL =
+  process.env.REACT_APP_CAR_RENTAL_BACKEND_URL || "http://localhost:3004";
 
 export default function Car() {
   const { id } = useParams();
-  const allCarsList = useContext(CarsContext);
+  const [currentCar, setCurrentCar] = useState();
+  const initBooking = {
+    carId: id,
+    email: "",
+    phone: "",
+  };
+  const [bookingInfo, setBookingInfo] = useState(initBooking);
+  // const allCarsList = useContext(CarsContext);
 
-  const currentCar = allCarsList.filter(
-    (car) => Number(car.id) === Number(id)
-  )[0];
-  console.log("all cars: ", allCarsList);
+  // const currentCar = allCarsList.filter(
+  //   (car) => Number(car.id) === Number(id)
+  // )[0];
+  // console.log("all cars: ", allCarsList);
+
+  useEffect(() => {
+    const getCurrentCar = async () => {
+      const result = await axios.get(`${BACKEND_URL}/cars/${id}`);
+      setCurrentCar(result.data.car);
+    };
+    getCurrentCar();
+  }, []);
 
   const handleSubmit = () => {
     console.log("submit done!");
@@ -20,26 +42,30 @@ export default function Car() {
   return (
     <div>
       <Navbar />
-      <div>
-        <h2>Car details: {id}</h2>
-        <h4>Name: {currentCar.name}</h4>
-        <p>Model: {currentCar.model}</p>
-        <p>Type: {currentCar.type}</p>
-        <p>Price: ${currentCar.price} / Day</p>
-      </div>
+      {currentCar && (
+        <div>
+          <h2>Car details: {id}</h2>
+          <h4>Name: {currentCar.name}</h4>
+          <p>Model: {currentCar.model}</p>
+          <p>Type: {currentCar.type}</p>
+          <p>Price: ${currentCar.price} / Day</p>
+        </div>
+      )}
+      <p>Please input your contact info to book: </p>
       <form onSubmit={handleSubmit}>
-        <label>Pickup Loaction</label>
-        <input />
-        <label>Start Date</label>
-        <input />
-        <label>End Date</label>
-        <input />
         <label>Your Email</label>
-        <input />
+        <input
+          value={bookingInfo.email}
+          onChange={(e) => setBookingInfo({ email: e.target.value })}
+        />
         <label>Your Contact No.</label>
-        <input />
+        <input
+          value={bookingInfo.phone}
+          onChange={(e) => setBookingInfo({ phone: e.target.value })}
+        />
+        <button>Book</button>
       </form>
-      <Link to={`/cars/${currentCar.id}/book`}>Book</Link>
+      {/* <Link to={`/cars/${currentCar.id}/book`}>Book</Link> */}
     </div>
   );
 }
